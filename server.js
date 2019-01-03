d14eab229454f22d21b9ce964da668fcd42c7e89
amandamurillo <amandasmurillo@gmail.com>
2019-01-03 13:07:34 -0800

    updated server and routes

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-//Your `server.js` file should require the basic npm packages we've used in class: `express` and `path`.
+//Your `server.js` file should require the basic npm packages we've used
+// in class: `express` and `path`.
+
+var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+
+//makes it easy for our server to interpret data sent to it- standard
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes that give our server a map of how to respond
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+app.listen(PORT,function(){
+
+    console.log("App listening on PORT :" + PORT);
+});
